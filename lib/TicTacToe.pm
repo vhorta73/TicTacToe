bc8d2f48a02635c826faec60145458ab486dc02c
Vasco Horta <diasfonseca@live.com>
2020-12-31 12:14:58 +0000

    Initial commit

---
+++ lib/TicTacToe.pm
@@ -0,0 +1,36 @@
+package TicTacToe;
+
+=head1 NAME
+
+TicTacToe - The TicTacTow build class.
+
+=head1 SYNOPSIS
+
+  perl Build.PL
+  Build test
+  Build install
+
+=head1 DESCRIPTION
+
+The TicTacToe game.
+
+=head1 DEFINITIONS
+
+=cut
+
+BEGIN {
+  use Exporter;
+  use vars     qw{ $VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS };
+  $VERSION     = '1.00';
+  push @ISA,   qw{ Exporter };
+ 
+  @EXPORT      = qw{};
+  @EXPORT_OK   = qw{};
+  %EXPORT_TAGS = ();
+}
+
+#-------------------------------------------------------------------------------
+1;
+#===============================================================================
+__END__
+#===============================================================================
